3c8873a4bc149cda7d200bd12bfd2d4f651d52a6
Ewerton L. Madruga <emadruga@Sierra-Nevada.local>
2018-08-14 20:38:14 -0300

    - full featured app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: '/checklists', pathMatch: 'full' },
+  { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
+  { path: 'checklists', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'checklists/:id', loadChildren: './checklist/checklist.module#ChecklistPageModule' }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
