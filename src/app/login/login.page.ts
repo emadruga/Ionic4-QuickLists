d31a6599679ddabe692f1ede83d6f3cfe39181c4
Ewerton L. Madruga <emadruga@Sierra-Nevada.local>
2018-08-19 20:11:43 -0300

    - login simulation when username has 5 alphanumeric chars.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-    public backgroundImage = 'assets/img/background/background-5.jpg'
-    constructor() { }
+    public backgroundImage = 'assets/img/background/background-5.jpg';
+    public username: string;
+    
+    constructor( private navCtrl: NavController ) { }
 
     ngOnInit() {
     }
     
     login() {
-	console.log("Let's do login...");
+	if (this.username !== undefined &&
+	    /^([a-z0-9]{5,})$/.test(this.username) ) {
+	    console.log("Let's do login for " + this.username);
+	    this.navCtrl.goForward('/checklists');
+	} else {
+	    console.log("No username set yet!");
+	}
     }
 
     launchSignup() {
