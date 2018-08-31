5028fb58569caf2116ab73775d5efc52be5a6973
Ewerton Madruga <emadruga@gmail.com>
2018-08-31 16:39:07 -0300

    - reshuffling layout and adding phone field.

--- src/app/new-applicant/new-applicant.page.ts
+++ src/app/new-applicant/new-applicant.page.ts
@@ -8,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 
 export class NewApplicantPage implements OnInit {
 
-    private ehPNE: boolean;
-    private sexo: string;
-    private data_nasc: string;
+    private nome_completo: string;
+    private data_nasc    : string;
+    private rg_identidade: string;
+    private cpf          : string;
+    private sexo         : string;
+    private email        : string;
+    private cidade       : string;
+    private cep          : string;
+    private telefone     : string;
+    private deficiencia  : string;
+    private cotista      : string;
 
     
     constructor() {
